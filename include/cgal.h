505180f6f3f8e9f29b2efd61bc4e7e4e1365f232
Observer46 <microplayerpl@gmail.com>
2021-03-25 00:58:11 +0100

    Added example project in OpenCV

---
+++ include/cgal.h
@@ -0,0 +1,10 @@
+//
+// Created by micro on 25.03.2021.
+//
+
+#ifndef DATA_PROCESSOR_CGAL_H
+#define DATA_PROCESSOR_CGAL_H
+
+int cgal_main(int argc, char* argv[]);
+
+#endif //DATA_PROCESSOR_CGAL_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,9 @@
-#define CGAL_USE_BASIC_VIEWER
-#include <boost/asio.hpp>
-#include <CGAL/Simple_cartesian.h>
-#include <CGAL/Surface_mesh.h>
-#include <CGAL/draw_surface_mesh.h>
-#include <fstream>
-#include <iostream>
-#include <filesystem>
-typedef CGAL::Simple_cartesian<double>                       Kernel;
-typedef Kernel::Point_3                                      Point;
-typedef CGAL::Surface_mesh<Point>                            Mesh;
+
+#include <cgal.h>
+#include <opencv.h>
+
 int main(int argc, char* argv[])
 {
-    boost::asio::io_context io_context;
-  Mesh sm1;
-  std::ifstream in1((argc>1)?argv[1]:"data/elephant.off");
-  std::cout << std::filesystem::current_path() << endl;
-  std::cout << in1.is_open() << std::endl;
-  in1 >> sm1;
-  CGAL::draw(sm1);
-  return EXIT_SUCCESS;
+    return opencv_main(argc, argv);
 }
+
